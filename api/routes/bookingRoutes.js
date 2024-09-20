e5fefe6deaa45e9f9e7678f024482e8a110148df
nagendrakodama123 <126648606+nagendrakodama123@users.noreply.github.com>
2024-09-20 14:11:53 +0530

    added booking and vehicle routes

--- api/routes/bookingRoutes.js
+++ api/routes/bookingRoutes.js
@@ -1,15 +1,8 @@
-module.exports = (sequelize, DataTypes) => {
-    const Booking = sequelize.define('Booking', {
-      vehicleId: DataTypes.INTEGER,
-      userId: DataTypes.INTEGER,
-      startDate: DataTypes.DATE,
-      endDate: DataTypes.DATE,
-    });
-  
-    Booking.associate = function(models) {
-      Booking.belongsTo(models.Vehicle, { foreignKey: 'vehicleId' });
-    };
-  
-    return Booking;
-  };
-  
+const express = require('express');
+const router = express.Router();
+const BookingController = require('../controllers/bookingController');
+
+router.post('/', BookingController.bookVehicle);
+router.get('/all', BookingController.getAll);
+
+module.exports = router;
--- api/routes/vehicleRoutes.js
+++ api/routes/vehicleRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const VehicleController = require('../controllers/vehicleController');
 
-// Route to get vehicles by number of wheels
-router.get('/vehicles/wheels/:noOfWheels', VehicleController.getVehiclesByNoOfWheels);
+router.get('/vehiclestypes/wheels/:noOfWheels', VehicleController.getVehiclesByNoOfWheels);
+router.get('/vehicles/:vehicleTypeId', VehicleController.getAllVehicles);
 
 module.exports = router;
